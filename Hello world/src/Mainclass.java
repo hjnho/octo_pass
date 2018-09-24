630e46d35a48dd05b199a475b1b4d0f43f73034a
노희진 <user@yonsei.ac.kr>
2018-09-24 16:47:47 +0900

    student class 추가

--- Hello world/src/Mainclass.java
+++ Hello world/src/Mainclass.java
@@ -2,5 +2,8 @@
 public class Mainclass {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		
+		student stu = new student();
+		
 	}
 }
---
+++ Hello world/src/student.java
@@ -0,0 +1,6 @@
+
+public class student {
+	public student() {
+		System.out.println("=====STUDENT=====");
+	}
+}
