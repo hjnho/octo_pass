b3b7fe12b5a9554c34f743d113f6b3fa51d19b5f
노희진 <user@yonsei.ac.kr>
2018-09-25 16:09:13 +0900

    BlackJack Example_1 내용 수정 ( 함수 내용 객체화)

--- Blackjack/src/blackjack_1/Card.java
+++ Blackjack/src/blackjack_1/Card.java
@@ -4,6 +4,11 @@ public class Card {
 	private String pattern;
 	private String denomination;
 	
+	public Card(String pattern, String denomination) {
+		this.pattern = pattern;
+		this.denomination = denomination;
+	}
+	
 	public String getPattern() {
 		return pattern;
 	}
@@ -18,6 +23,12 @@ public class Card {
 	
 	public void setDenomination(String denomination) {
 		this.denomination = denomination;
+	}
+	
+	@Override
+	public String toString() {
+		return "Card{"+"pattern='"+pattern+", "
+				+ "denomination='"+denomination+'}';
 	}
 }
 
--- Blackjack/src/blackjack_1/Game.java
+++ Blackjack/src/blackjack_1/Game.java
@@ -9,5 +9,7 @@ public class Game {
 		Rule rule = new Rule();
 		CardDeck cardDeck = new CardDeck();
 		
+		System.out.println(cardDeck.toString());
+		
 	}
 }
